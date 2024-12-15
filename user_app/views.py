8b9d6c8184cfd636a836f0424d47adf07a28f17c
Javlonbek0205 <javlonbekdeveloper7@gmail.com>
2024-12-15 16:53:18 +0500

    Initial commit User Authentication

---
+++ user_app/views.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import User
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    user = request.user
+    context = {'user': user}
+    return render(request, 'home.html', context)
